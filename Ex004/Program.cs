dca77126879d89eedcec17811e437946d95d7521
Beyer_Anton <anton.beyer88@gmail.com>
2022-08-10 20:34:26 +0700

    ex005

--- Ex004/Program.cs
+++ Ex004/Program.cs
@@ -1,2 +1,6 @@
-﻿
-Console.WriteLine("go last");
+﻿int numberA = new Random().Next(1, 10); // 1 2 3 .... 9
+Console.Write (numberA);
+int numberB = new Random().Next(1, 10); // 1 2 3 .... 9
+Console.Write (numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
